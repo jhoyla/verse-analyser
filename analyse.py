ce9a46ef31ba92f527dea268e0aff907c3862699
Jonathan Hoyland <jonathan.hoyland@gmail.com>
2020-03-09 01:02:35 +0000

    Add parsing for verses, including support for excluded manuscripts.

--- analyse.py
+++ analyse.py
@@ -1,4 +1,13 @@
 import csv
+import re
+
+pair = {'*':'m','m':'*','a':'°', '°':'a'}
+
+def parse_group(group):
+    patt = re.compile('^\s*(([0-9]+[am°\*]?\s*,\s*)*[0-9]+[am°\*]?)?\s*$')
+    if patt.match(group) == None:
+        raise SyntaxError("Group is not in correct format: {grp}".format(grp=group))
+    return [x.strip() for x in group.split(',') if x.strip() != '']
 
 def parse_verse(filename, manuscripts):
     verse = []
@@ -6,13 +15,41 @@ def parse_verse(filename, manuscripts):
         i = csv.reader(cr, delimiter='/', quotechar='"')
         for row in i:
             verse.append(row)
-    readings = []
+    blanks = []
+    if verse[-2] == '---' or '\'---':
+        blanks = parse_group(verse[-1][0])
+        verse = verse[:-2]
+    for blank in blanks:
+        manuscripts.remove(blank)
+
+    words = []
+
     for word in verse:
-        groups = []
-        for group in word:
-            groups.append([x.strip() for x in group.split(',')])
-        readings.append(groups)
-    return readings
+        readings = [parse_group(reading) for reading in word if len(parse_group(reading)) > 0  ]
+        words.append(readings)
+
+    is_normal = re.compile('^[0-9]+$')
+    is_special = re.compile('^[0-9]+[m\*a°]$')
+    for word in words:
+        ms_stack = manuscripts.copy()
+        for reading in word:
+            for elem in reading:
+                if is_normal.match(elem) == None:
+                    if is_special.match(elem) == None:
+                        raise SyntaxError('Bad manuscript: {man}'.format(man=elem))
+                    else:
+                        base_manuscript = elem[:-1]
+                        modifier = elem[-1]
+                        if base_manuscript not in manuscripts:
+                            raise LookupError('Unknown manuscript: {man}'.format(man=elem))
+                        ms_stack.remove(base_manuscript)
+                        pair_manuscript = base_manuscript + pair[modifier]
+                        ms_stack.add(pair_manuscript)
+                else:
+                    ms_stack.remove(elem)
+        word.append(list(ms_stack))
+    return words        
+
 
 def parse_manuscripts(filename):
     ms = []
@@ -20,4 +57,7 @@ def parse_manuscripts(filename):
         i = csv.reader(cr, delimiter=',', quotechar='"')
         for row in i:
             ms.append(row)
-    return ms[0]
+    return set(ms[0])
+
+def update_table(table, word):
+    pass
